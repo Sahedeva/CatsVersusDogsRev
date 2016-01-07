dacd6c47936d9186e2223cefa3b3ecf8cbb83be8
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2016-01-07 17:45:05 -0600

    Fixed message after 2nd player chooses avatar

--- JS_files/catVsDogFinal.js
+++ JS_files/catVsDogFinal.js
@@ -96,6 +96,7 @@ $(function () {
 				localStorage.setItem("firstPlayerwins", 0);
 				localStorage.setItem("secondPlayerwins", 0);
 			}
+			$('#playOrder').html("Press the Play Game button to enter the combat arena");
 			$("#pageLink").toggle();
 		});
 
